bdd11300ee1aea61ffdf9e0623f6cfb807f5ecee
Dan Giuliani <Dan Giuliani>
2016-02-20 16:23:20 -0800

    oxford

--- server.js
+++ server.js
@@ -1,32 +1,24 @@
 // var express = require('express');
 // var app = express();
-// var oxford = require('project-oxford')
-// var client = new oxford.Client('c6a56dbc2117468ba20aed22858b8454');
-
-// var port = process.env.PORT || 9999;
-
-// client.face.detect({
-//     path: 'public/images/lebron.jpg',
-//     analyzesAge: true,
-//     analyzesGender: true
-// }).then(function (response) {
-//     console.log(response[0]);
-//     console.log('The age is: ' + response[0].faceAttributes.age);
-//     console.log('The gender is: ' + response[0].faceAttributes.gender);
-// });
-
-// app.get('/', function (req, res) {
-//   res.send('Hello World!');
-// });
-
-// app.listen(port, function () {
-//   console.log('Example app listening on port 9999!');
-// });
-
+var oxford = require('project-oxford')
+var client = new oxford.Client('c6a56dbc2117468ba20aed22858b8454');
 var http = require('http')
 var port = process.env.PORT || 1337;
 var url = require('url');
+
+var faceDetect = function(res) {
+    client.face.detect({
+    path: 'public/images/lebron.jpg',
+    analyzesAge: true,
+    analyzesGender: true
+    }).then(function (response) {
+        console.log(response[0]);
+        res.end('The age is: ' + response[0].faceAttributes.age);
+        console.log('The gender is: ' + response[0].faceAttributes.gender);
+    });
+}
+
 http.createServer(function(req, res) {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end("This is new text");
+  faceDetect(res);
 }).listen(port);
